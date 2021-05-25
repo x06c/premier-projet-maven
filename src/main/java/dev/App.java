0824c9a1ebb7b15f9f18ff621527d9dea83ab414
x06c <jonathan.miss@outlook.com>
2021-05-25 17:39:50 +0200

    commit 25/05 - TODO

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,11 +1,22 @@
 package dev;
 
+import com.github.lalyos.jfiglet.FigletFont;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
+        String asciiArt = null;
+        try {
+            asciiArt = FigletFont.convertOneLine("hello");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(asciiArt);
+
         SpringApplication.run(App.class, args);
 
 
